5d6c0c92543e2737779c786c40e9b108290069b5
Mithula Chanthuka <mithula.cbw@gmail.com>
2024-07-15 11:10:57 +0530

    Style buttons for player choices

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log("it's connected!")
 const rock=1;
 const paper=2;
 const scissors=3;
+let humanWinCount =0;
+let pcWinCount =0;
 
 function getRandom(){
     return Math.floor(Math.random() * 3) + 1;
@@ -11,3 +13,4 @@ function getRandom(){
 // for(let i=0; i<100;i++){
 //     console.log(getRandom());
 // }
+
