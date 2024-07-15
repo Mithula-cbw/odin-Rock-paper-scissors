dff78c8b13c788563f8d6eca2cf4e190e28d1e4f
Mithula Chanthuka <mithula.cbw@gmail.com>
2024-07-15 10:04:25 +0530

    Get random choice between 1 and 3

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("it's connected!")
 
+const rock=1;
+const paper=2;
+const scissors=3;
+
+function getRandom(){
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+// for(let i=0; i<100;i++){
+//     console.log(getRandom());
+// }
